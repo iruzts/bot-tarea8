b80a25470b887cf74fb3be781ae489516aa48d92
iruzts <ruizsoto1996@gmail.com>
2021-08-01 15:55:31 -0600

    coreccion de error2

--- bot8.py
+++ bot8.py
@@ -1,7 +1,7 @@
 import telebot # importamos la libreria de telebot
 
 # token = os.getenv['TOKEN']
-bot = telebot.TeleBot('') #anadimos el token
+bot = telebot.TeleBot('1746473976:AAEnVcmQzO-hZmaMrG1c-6Vqc1zG9Qo6Qxg') #anadimos el token
 updates = bot.get_updates()
 message = updates[0].message
 from_user = message.from_user
@@ -12,18 +12,18 @@ get_me = bot.get_me()
 def hola(mensaje):
     bot.send_chat_action(id, 'typing')
     bot.send_message(id, "Envia 1 o Celsius par ver Formula Grados Celsius a Grados Kelvin \n Envia 2 o Kelvin  para ver Formula Grados Kelvin a Grados Celsius..")
-    print("Mandaron 1")
+    print("Mandaron info")
 
 @bot.message_handler(commands=['1','Celsius']) 
-def hola(mensaje):
+def celsius(mensaje):
     bot.send_chat_action(id, 'typing')
-    bot.send_message(id, ("0 °C + 273.15 = 273.15 K")
+    bot.send_message(id, "0 °C + 273.15 = 273.15 K")
     print("Mandaron 1")
 
-@bot.message_handler(commands=['2','Kelvin '])
-def nombre(mensaje):
+@bot.message_handler(commands=['2','Kelvin'])
+def kelvin(mensaje):
     bot.send_chat_action(id, 'typing')
-    bot.send_message(id, ("0 K − 273.15 = -273.1 °C")
+    bot.send_message(id, "0 K − 273.15 = -273.1 °C")
     print("Mandaron 2")
 
 bot.polling()
